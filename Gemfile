ee5f0ec150b099ae6df42fe0d09f3d985a416bd1
hong7463 <hison7463@gmail.com>
2015-04-25 01:10:10 -0400

    Add Association

--- Gemfile
+++ Gemfile
@@ -43,5 +43,8 @@ group :development, :test do
   gem 'spring'
   gem 'haml', '~> 4.0.6'
   gem 'simple_form', '~> 3.1.0'
+  gem 'bootstrap-sass', '~> 3.3.4.1'
+  gem 'paperclip', '~> 4.2.1'
+  gem 'cocoon', '~> 1.2.6'
 end
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to @recipe
+      redirect_to @recipe, notice: "Successfully created new recipe"
     else
       render 'new'
     end 
@@ -24,15 +24,23 @@ class RecipesController < ApplicationController
   end 
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Successfully changed recipe"
+    else 
+      render 'edit'
+    end 
   end 
 
   def destroy
+    @recipe.destroy
+
+    redirect_to root_path
   end 
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
   end 
 
   def find_recipe
